15ce2b095669f5049afcbc839d633619c320b9c5
Nicholle Verdugo <nicolvgil@hotmail.com>
2016-03-30 14:37:22 -0500

    Lista de usuarios

---
+++ usuarios.py
@@ -0,0 +1,15 @@
+import commands
+import json
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def usuarios():
+
+	ret=commands.getoutput("ls /home | sort")
+	ret=ret.split("\n")
+	ret="{users: "+json.dumps(ret)+"}"
+	return ret
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', debug=True)
